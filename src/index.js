0057ec38b6eead778a22d7f938772f374b316421
Алина <alina@MacBook-Pro-Alina.local>
2020-10-19 13:12:05 +0300

    feat: add function index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    if (typeof n !== 'number') {
+        return;
+    }
+    if (n < 0) {
+        n = -n;
+    }
+    let num = String(n);
+    return num.split("").reverse().join("");
+
 }
